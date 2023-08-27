7f78659979540cb20cb73911b5eb2be1ef0f1ba7
Onyinyechi Susan Madu <successfulsusan2014@gmail.com>
2023-08-27 02:55:12 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,30 +1,30 @@
-#include "lists.h"
-#include <string.h>
 #include <stdlib.h>
-/**
-* _strlen - Gives the length of a string.
-* @s: String to evaluate.
-* Return: The length of the given string.
-*/
-list_t *add_node_end(list_t **head, const char *str)
-{
-	list_t *new, *h = *head;
+#include <string.h>
+#include "lists.h"
 
-	if (!head)
-		return (NULL);
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
 	new = malloc(sizeof(list_t));
 	if (!new)
-		return (0);
+		return (NULL);
+
 	new->str = strdup(str);
-	new->len = _strlen(new->str);
-	new->next = 0;
-	if (!*head)
-	{
-		*head = new;
-		return (new);
-	}
-	while (h->next)
-		h = h->next;
-	h->next = new;
-	return (new);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
 }

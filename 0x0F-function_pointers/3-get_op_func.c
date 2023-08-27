2c3ab775643e4505c29fcd0b48e0c381b0ee20de
Onyinyechi Susan Madu <successfulsusan2014@gmail.com>
2023-08-27 04:27:04 -0700

    3-main

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,12 +1,16 @@
 #include "3-calc.h"
+
 /**
- * get_op_func - matches operator from main
- * @s: op str
- * Return: 0
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t op_s[] = {
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -14,14 +18,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i;
 
-	int i = 0;
+	i = 0;
 
-	while (op_s[i].op)
+	while (ops[i].op)
 	{
-		if (*(op_s[i].op) == *s)
-			return (op_s[i].f);
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,20 +1,17 @@
 #include "3-calc.h"
+
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -22,25 +19,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
+	result = func(arg1, arg2);
 
-	printf("%d\n", ans);
+	printf("%d\n", result);
+
 	return (0);
 }
